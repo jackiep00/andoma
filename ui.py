9cc2aca62a28dafee525d81458a0633ebdb222f4
Jack Yin <jackyin0@gmail.com>
2021-06-20 02:12:55 -0400

    demo game available

--- ui.py
+++ ui.py
@@ -5,15 +5,34 @@ import argparse
 import chess.svg
 from IPython.display import SVG, display
 from hab_movegeneration import next_move
-from bots.example_bots import bot_choose_piece
-from helpers import piece_to_string
+from helpers import piece_to_string, get_legal_pieces
+from typing import Callable
 
 piece_mapping = {
     'p': 1, 'n': 2, 'b': 3, 'r': 4, 'q': 5, 'k': 6, 'pawn': 1, 'knight': 2, 'bishop': 3, 'rook': 4, 'queen': 5, 'king': 6
 }
 
 
-def start():
+def get_constraint(board: chess.Board) -> chess.Piece:
+    """
+    Try (and keep trying) to get a legal bot constraint from the user.
+    """
+    # in the case where this is a human looking at what move they should do next
+    # show them the board to give them a chance
+    display(SVG(chess.svg.board(board, size=275, orientation=board.turn)))
+
+    move = input(
+        f"\nThe constraint for the bot (Options: {list(set([piece_to_string(x) for x in get_legal_pieces(board)]))}:\n")
+
+    for legal_piece in get_legal_pieces(board):
+        if piece_mapping.setdefault(move.lower(), None) == legal_piece:
+            return legal_piece
+
+    print('Invalid piece choice')
+    return get_constraint(board)
+
+
+def start(choose_piece: Callable = get_constraint):
     """
     Start the command line user interface.
     """
@@ -31,7 +50,7 @@ def start():
         board.push(get_move(board))
 
     while not board.is_game_over():
-        constraint = bot_choose_piece(board)
+        constraint = choose_piece(board)
         print(f'Bot Chose {piece_to_string(constraint)}')
         board.push(next_move(get_depth(), board,
                    debug=False, piece_constraint=constraint))
@@ -42,39 +61,6 @@ def start():
         board.push(get_move(board))
 
     print(f"\nResult: [w] {board.result()} [b]")
-
-
-def render(board: chess.Board) -> str:
-    """
-    Print a side-relative chess board with special chess characters.
-    """
-    board_string = list(str(board))
-    uni_pieces = {
-        "R": "♖",
-        "N": "♘",
-        "B": "♗",
-        "Q": "♕",
-        "K": "♔",
-        "P": "♙",
-        "r": "♜",
-        "n": "♞",
-        "b": "♝",
-        "q": "♛",
-        "k": "♚",
-        "p": "♟",
-        ".": "·",
-    }
-    for idx, char in enumerate(board_string):
-        if char in uni_pieces:
-            board_string[idx] = uni_pieces[char]
-    ranks = ["1", "2", "3", "4", "5", "6", "7", "8"]
-    display = []
-    for rank in "".join(board_string).split("\n"):
-        display.append(f"  {ranks.pop()} {rank}")
-    if board.turn == chess.BLACK:
-        display.reverse()
-    display.append("    a b c d e f g h")
-    return "\n" + "\n".join(display)
 
 
 def get_move(board: chess.Board) -> chess.Move:
@@ -90,21 +76,6 @@ def get_move(board: chess.Board) -> chess.Move:
 
     print('Invalid move')
     return get_move(board)
-
-
-def get_constraint(board: chess.Board) -> chess.Piece:
-    """
-    Try (and keep trying) to get a legal bot constraint from the user.
-    """
-    move = input(
-        f"\nThe constraint for the bot (e.g. {list(board.legal_moves)[0]}):\n")
-
-    for legal_move in board.legal_moves:
-        if move == str(legal_move):
-            return legal_move
-
-    print('Invalid move')
-    return get_constraint(board)
 
 
 def get_depth() -> int:
