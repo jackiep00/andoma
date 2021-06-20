398d8eecb4e35c3033bf87635c27ec8c2ea265bd
Jack Yin <jackyin0@gmail.com>
2021-06-20 14:23:05 -0400

    attackers and defenders

--- bots/example_bots.py
+++ bots/example_bots.py
@@ -16,3 +16,21 @@ def choose_piece_in_order(board: chess.Board) -> chess.Piece:
     legal_pieces.sort()
 
     return legal_pieces[0]
+
+
+def two_knights_opening(board: chess.Board) -> chess.Piece:
+    # first two moves, move the knight
+    if (board.fullmove_number <= 2):
+        return chess.KNIGHT
+    if (board.fullmove_number <= 4):
+        return chess.PAWN
+
+
+def count_attackers_and_defenders(board: chess.Board) -> chess.Piece:
+    my_color = board.turn
+    candidate_moves = []
+    for move in list(board.legal_moves()):
+        num_attackers = len(board.attackers(my_color, move.to_square))
+        num_defenders = len(board.attackers(not my_color, move.to_square))
+        if num_attackers > num_defenders:
+            candidate_moves.push(move)
